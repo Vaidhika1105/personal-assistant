880b5671cbec1caa85cbd403d0153906dcbfebeb
Vaidhika Mateti <201702803+Vaidhika1105@users.noreply.github.com>
2026-07-26 22:46:15 +0530

    Update test_webhook.py

--- test_webhook.py
+++ test_webhook.py
@@ -5,7 +5,7 @@ user_message =  "Can you tell me about blach holes in 3-4 lines"
 
 request_message = {"message" : user_message}
 
-url = "https://vaidhika-11.app.n8n.cloud/webhook/63c04f9d-3c81-48f9-88b9-7ea1f52291db"
+url = "PASTE_YOUR_WEBHOOK_URL"
 
 response = requests.post(url, json=request_message)
 
